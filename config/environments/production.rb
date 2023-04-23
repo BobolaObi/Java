4d225b3cc47fd0a411fbc0cb46506d9a75c9bb23
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-04-23 02:01:46 -0400

    Auth: to kenerated domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.hosts << "0.0.0.0:3000"
-  
+  config.hosts << "java-production-9f69.up.railway.app"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
